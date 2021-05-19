ad5b6d88f497164fe7be3b822be229efc2da72c1
Jay Werra <jay@jaywerra.com>
2021-05-18 22:15:05 -0700

    Removing external link icons

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,60 +1,64 @@
-import React from "react"
+import React, {useEffect} from "react"
 import { Link } from 'gatsby'
 import Obfuscate from "react-obfuscate"
 import { LinkList } from "../Header/HeaderStyles"
 import ExternalLink from "../../images/icons/icon-externallink.svg"
 
 const Nav = () => {
-  return (
-    <LinkList>
-        <li>
-            <Link
-                to="/"
-                activeStyle={{ color: "#0032BD" }}
-            >
-                Projects
-            </Link>
+    const activeStyles = {
+        color: "#2700CA"
+    }
 
-        </li>
-        <li>
-            <Link
-                to="/resume/"
-                activeStyle={{ color: "#0032BD" }}
-            >
-                Resume
-            </Link>
-        </li>
-        <li>
-            <Obfuscate email="jay@jaywerra.com" aria-label="Email Me">
-                Email
-            </Obfuscate>
-        </li>
+    return (
+        <LinkList>
             <li>
-                <a href="https://werra.io" target="_blank" rel="noopener noreferrer">
-                    Werra
-                    <img src={ExternalLink} alt="" />
-                </a>
+                <Link
+                    to="/"
+                    activeStyle={activeStyles}
+                >
+                    Projects
+                </Link>
+
             </li>
             <li>
-                <a href="https://github.com/werradesign" target="_blank" rel="noopener noreferrer">
-                    GitHub
-                    <img src={ExternalLink} alt="" />
-                </a>
+                <Link
+                    to="/resume/"
+                    activeStyle={activeStyles}
+                >
+                    Resume
+                </Link>
             </li>
             <li>
-                <a href="https://www.instagram.com/jaywerra/" target="_blank" rel="noopener noreferrer">
-                    Instagram
-                    <img src={ExternalLink} alt="" />
-                </a>
+                <Obfuscate email="jay@jaywerra.com" aria-label="Email Me">
+                    Email
+                </Obfuscate>
             </li>
-            <li>
-                <a href="https://www.linkedin.com/in/jaywerra/" target="_blank" rel="noopener noreferrer">
-                    Linkedin
-                    <img src={ExternalLink} alt="" />
-                </a>
-            </li>
-    </LinkList>
-  )
+                <li>
+                    <a href="https://werra.io" target="_blank" rel="noopener noreferrer">
+                        Werra
+                        <img src={ExternalLink} alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/werradesign" target="_blank" rel="noopener noreferrer">
+                        GitHub
+                        <img src={ExternalLink} alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/jaywerra/" target="_blank" rel="noopener noreferrer">
+                        Instagram
+                        <img src={ExternalLink} alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/jaywerra/" target="_blank" rel="noopener noreferrer">
+                        Linkedin
+                        <img src={ExternalLink} alt="" />
+                    </a>
+                </li>
+        </LinkList>
+    )
 }
 
 export default Nav
